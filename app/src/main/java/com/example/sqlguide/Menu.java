3ae1a9340de6bb9d897e722f8a221690c79567fb
Diego <diegogabriel07perez@gmail.com>
2023-09-26 15:03:08 -0600

    Se agrego la primera leccion con una tabla

--- app/src/main/java/com/example/sqlguide/Menu.java
+++ app/src/main/java/com/example/sqlguide/Menu.java
@@ -2,7 +2,10 @@ package com.example.sqlguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Menu extends AppCompatActivity {
 
@@ -11,4 +14,8 @@ public class Menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
     }
+
+    public void leccion1(View view){
+        Intent d=new Intent(this, Leccion1.class);
+        startActivity(d);}
 }
