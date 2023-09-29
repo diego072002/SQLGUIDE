2981311177c1c7ff364cbd3db4ba643f54a3a7bf
Diego <diegogabriel07perez@gmail.com>
2023-09-29 13:14:46 -0600

    Se creo el activity de la segunda leccion

---
+++ app/src/main/java/com/example/sqlguide/Leccion2.java
@@ -0,0 +1,14 @@
+package com.example.sqlguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Leccion2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leccion2);
+    }
+}
